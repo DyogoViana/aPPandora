bfe0ff37bd168ee83e73e5c58d56b0d70304f218
Dyogo Viana <dyogo.vianaa@gmail.com>
2020-05-11 19:27:44 -0300

    Adicionando repositórios / Animação do loading.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,22 +1,66 @@
-import React from 'react';
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 import { Container, Form, SubmitButton } from './styles';
 
-export default function Main() {
-	return (
-		<Container>
-			<h1>
-				<FaGithubAlt />
-				Repositórios
-			</h1>
+import api from '../../services/api';
 
-			<Form onSubmit={() => {}}>
-				<input type="text" placeholder="Adicionar repositório" />
+export default class Main extends Component {
+	state = {
+		newRepo: '',
+		repositories: [],
+		loading: false, // desabilita o btn de add, enquanto tá carregando um repositório
+	};
 
-				<SubmitButton disable>
-					<FaPlus color="#fff" size={14} />
-				</SubmitButton>
-			</Form>
-		</Container>
-	);
+	handleInputChange = (e) => {
+		this.setState({ newRepo: e.target.value });
+	};
+
+	handleSubmit = async (e) => {
+		e.preventDefault();
+
+		this.setState({ loading: true });
+
+		const { newRepo, repositories } = this.state;
+		const response = await api.get(`/repos/${newRepo}`);
+		const data = {
+			name: response.data.full_name,
+		};
+
+		this.setState({
+			repositories: [...repositories, data],
+			newRepo: '',
+			loading: false,
+		});
+
+		console.log(response.data);
+	};
+
+	render() {
+		const { newRepo, loading } = this.state;
+		return (
+			<Container>
+				<h1>
+					<FaGithubAlt />
+					Repositórios
+				</h1>
+
+				<Form onSubmit={this.handleSubmit}>
+					<input
+						type="text"
+						placeholder="Adicionar repositório"
+						value={newRepo}
+						onChange={this.handleInputChange}
+					/>
+
+					<SubmitButton loading={loading}>
+						{loading ? (
+							<FaSpinner color="#fff" size={14} />
+						) : (
+							<FaPlus color="#fff" size={14} />
+						)}
+					</SubmitButton>
+				</Form>
+			</Container>
+		);
+	}
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 export const Container = styled.div`
 	max-width: 700px;
@@ -34,9 +34,16 @@ export const Form = styled.form`
 	}
 `;
 
-export const SubmitButton = styled.button.attrs({
+// Animação de giro. Ex: loading.
+const rotate = keyframes`
+	from { transform: rotate(0deg); }
+	to { transform: rotate(360deg); }
+`;
+
+export const SubmitButton = styled.button.attrs((props) => ({
 	type: 'submit',
-})`
+	disabled: props.loading,
+}))`
 	background: #7159c1;
 	border: 0;
 	padding: 0 15px;
@@ -46,4 +53,19 @@ export const SubmitButton = styled.button.attrs({
 	display: flex;
 	justify-content: center;
 	align-items: center;
+
+	/* Desativa o btn quando tiver add/carregando um novo repositório. */
+	&[disabled] {
+		cursor: not-allowed;
+		opacity: 0.6;
+	}
+
+	/* Giro do icone de loading */
+	${(props) =>
+		props.loading &&
+		css`
+			svg {
+				animation: ${rotate} 2s linear infinite;
+			}
+		`}
 `;
